70c7d8ceb30dacb39a374bbdf457d47faea879ee
Arnaud DEMARCQ <arnaud.demarcq@laposte.net>
2014-04-23 20:49:20 +0200

    Adds Suppresions to checkstyle configuration

--- PocketWorlds/webapps/build.sh
+++ PocketWorlds/webapps/build.sh
@@ -1,1 +1,1 @@
-mvn clean install checkstyle:checkstyle-aggregate javadoc:aggregate pmd:pmd -Dcheckstyle.config.location=project_checks.xml 
+mvn clean install checkstyle:checkstyle-aggregate javadoc:aggregate pmd:pmd -Dcheckstyle.config.location=checkstyle_config.xml -Dcheckstyle.suppressions.location=checkstyle_suppression.xml
